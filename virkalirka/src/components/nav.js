e9411ff7eeb1c2ee603adb2af84201dcc65ed8ee
CupcakeNemi <silje.straum@gmail.com>
2023-05-03 10:32:26 +0200

    moved images to frontend map

--- virkalirka/src/components/nav.js
+++ virkalirka/src/components/nav.js
@@ -1,33 +1,38 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
+import headerImg from '../images/crochetsquare.jpg';
 
 const Nav = () => {
-    const {logout} = useLogout();
-    const {user} = useAuthContext()
+    const { logout } = useLogout();
+    const { user } = useAuthContext()
     const handleClick = () => {
         logout()
     }
     return (
         <header>
+            <div>
+                <img src={headerImg} alt="bunny and pink penguin" id="header-img"/>
+                <h1>Virkalirka</h1>
+            </div>
             <div className="navContainer">
-            <Link to="/">
-                <h1>VirkaLirka Nav</h1>
-            </Link>
-        <nav>
-            {user &&(
-                <div>
-                    <span>{user.username}</span>
-                    <button onClick={handleClick}>Log out</button>
-                </div>
-            )}
-            {!user &&(
-                <div>
-                    <Link to="/login">Login</Link>
-                    <Link to="/signup">Signup</Link>
-                </div>
-            )}
-        </nav>
+                <Link to="/">
+                    <h1>VirkaLirka Nav</h1>
+                </Link>
+                <nav>
+                    {user && (
+                        <div>
+                            <span>{user.username}</span>
+                            <button onClick={handleClick}>Log out</button>
+                        </div>
+                    )}
+                    {!user && (
+                        <div>
+                            <Link to="/login">Login</Link>
+                            <Link to="/signup">Signup</Link>
+                        </div>
+                    )}
+                </nav>
             </div>
         </header>
     )
