c4e290ba4ed154cbc5229d36c6b726fc686091af
CupcakeNemi <silje.straum@gmail.com>
2023-05-27 18:26:09 +0200

    me being stupid

--- virkalirka/src/backendURL.js
+++ virkalirka/src/backendURL.js
@@ -1,3 +1,3 @@
-// const URL = "http://139.144.78.54:4000";
-const URL = "http://localhost:4000";
+const URL = "http://139.144.78.54:4000";
+// const URL = "http://localhost:4000";
 export default URL; 
